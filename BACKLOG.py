98232aa6322df46020adf43a02e5c2164853e791
2om6i <bercin01@iba.dk>
2025-11-27 12:08:34 +0100

    data

--- BACKLOG.py
+++ BACKLOG.py
@@ -5,11 +5,12 @@ import numpy as np
 
 #Indlæs data fra CSV-filen
 df = pd.read_csv("antalbesoegende.csv", sep=";",encoding="utf-8") #sep=";" fordi filen bruger semikolon som separator og encoding="latin1" fordi der er æ/ø/å i kolonnenavne
-dt = pd.read_csv("salgdata.csv", sep=";", encoding="latin1")
-dg = pd.read_csv("netto_pr_kvartal.csv", sep=";", encoding="latin1")
+do = pd.read_csv("salgdata.csv", sep=";", encoding="latin1")
+dg = pd.read_csv("netto_pr_kvartal.csv", sep=";", encoding="utf-8")
 
 
-print(df.columns) #viser hvad kolonnerne faktisk hedder
+print (dg.columns)
+#print(df.columns) #viser hvad kolonnerne faktisk hedder
 
 # dark mode
 plt.style.use('seaborn-v0_8-darkgrid')
@@ -28,7 +29,22 @@ plt.figure(figsize=(10, 5)) #gør figuren bredere, så datoer er nemme at læse
 
 plt.plot(df["Month"], df["Besoegende i webshoppen"], marker="o", label="Besøgende") #marker="o" tegn en lille cirkel på hvert datapunkt
 #linje for besøgende 
-#plt.gca().set_facecolor('#eafff5')
+
+
+
+# Lav figuren større så der er plads
+fig, ax = plt.subplots(figsize=(12, 6))
+
+# Sorter efter kvartal (så grafen vises i rigtig rækkefølge)
+#dg = dg.sort_values(['Kvartal'])
+
+# Lav søjlediagram (bar plot)
+bars = ax.bar(dg['Kvartal'], dg['Gross_revenue'], 
+              color='blue', edgecolor='black', alpha=0.7)
+# Tilføj titler og labels
+ax.set_title("Nettoomsætning pr. Kvartal", fontsize=16, fontweight='bold')
+ax.set_xlabel("Kvartal", fontsize=12)
+ax.set_ylabel("Nettoomsætning", fontsize=12)
 
 #titel, akser og forklaring
 plt.title("Udvikling i besøgende pr. måned") 
@@ -36,24 +52,11 @@ plt.xlabel("Måned")
 plt.ylabel("Antal")                                     
 plt.legend() #viser hvilken linje der er hvad
 
-# Lav figuren større så der er plads
-fig, ax = plt.subplots(figsize=(12, 6))
-
-# Sorter efter kvartal (så grafen vises i rigtig rækkefølge)
-dg = dg.sort_values(['Kvartal'])
-
-# Lav søjlediagram (bar plot)
-bars = ax.bar(dg['Kvartal'], dg['Nettoomsætning'], 
-              color='blue', edgecolor='black', alpha=0.7)
-# Tilføj titler og labels
-ax.set_title("Nettoomsætning pr. Kvartal", fontsize=16, fontweight='bold')
-ax.set_xlabel("Kvartal", fontsize=12)
-ax.set_ylabel("Nettoomsætning", fontsize=12)
 
 #gør datopænere
 plt.xticks(rotation=45) #roterer dato-teksterne så de ikke overlapper
 plt.tight_layout() #justerer layout så intet bliver klippet
 
 
-print (dg.columns)
+
 plt.show() 
