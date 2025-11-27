b6ed9271b9eb51f110d07ae225f0a01599012fd5
mortenbentzon <morben01@iba.dk>
2025-11-27 18:47:25 +0100

    Update besøgende.py

--- besøgende.py
+++ besøgende.py
@@ -3,37 +3,44 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import matplotlib.dates as mdates
 
+#DB file
 DB_FILE = 'database.db'
+#Query til at trække data fra SQL
 QUERY = 'SELECT Month, Visitors FROM visitors ORDER BY Month'
-
+#Opsætning til SQL
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
+#Trækker data fra QUERY til data variablen
 cursor.execute(QUERY)
 data = cursor.fetchall()
-
+#Lukker forbindelse til SQL grundet best parctice
 connection.close()
+#Variable to at holde dato efter den er sorteret i rækkefølge
 processed_data = []
 
+#For loop til at lægge dato og visitor i variabler til matplot
 for row in data:
+    #Dato før konvatering til dato format.
     date_string = row[0]
     
-
     if date_string:
-
-        date_obj = datetime.strptime(date_string, '%d-%m-%Y') 
+        #Omdanner en string til dato format til matplotlib
+        date_obj = datetime.strptime(date_string, '%d-%m-%Y')
         visitor_count = row[1]
         
-
+        #Ligger data i processed_data variablen efter konvatering
         processed_data.append((date_obj, visitor_count))
 
+#sorter første collomn i processed_data (dato) i rækkefølge så det bliver vist rigtgt i graffen
 processed_data.sort(key=lambda x: x[0])
-
+#definere hvad data der er måned
 months = [item[0] for item in processed_data]
+#Definere hvad data der er besøgende
 visitors = [item[1] for item in processed_data]
 
 plt.style.use('seaborn-v0_8-darkgrid')
-plt.figure(figsize=(10, 6))
+#plt.figure(figsize=(10, 10))
 
 plt.plot(months, visitors, linestyle='--', marker='s')
 
