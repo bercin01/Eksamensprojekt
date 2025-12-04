9220126b8a546cf2036481499688d373ebebca07
lashan03 <lashan03@iba.dk>
2025-12-04 18:21:18 +0100

    Update

--- test_dashboard.py
+++ test_dashboard.py
@@ -318,7 +318,7 @@ canvas_widget4.grid(row=0, column=0, sticky=NSEW)
 #c.CTkLabel(graf4, text="Mindst populære produkter").pack(pady=10)
 
 # Knap til at se markedsføring
-marketing_button = c.CTkButton(window, text="Se markedsføring", font=("Arial", 15), command=marketing_button, fg_color="#ff66c4", text_color="black", hover_color="#54ac80") # Laver knappen
+marketing_button = c.CTkButton(window, text="Se markedsføring", font=("Times New Roman", 15), command=marketing_button, fg_color="#ff66c4", text_color="black", hover_color="#54ac80") # Laver knappen
 marketing_button.grid(row=0, column=2, padx= 30, pady=20, sticky="e") # Placerer den op i Højre hjørne, ændret til column 2
 
 window.mainloop()
